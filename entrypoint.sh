9d1f094ba9733cc4b3ee17e4aeca8cf0e648ce83
Ralina <seitovaralina1@gmail.com>
2025-10-13 10:42:45 +0600

    refactor: simplify entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 
-echo "Waiting for PostgreSQL to start..."
-while ! python -c "
-import socket
-import sys
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    result = sock.connect_ex(('db', 5432))
-    sock.close()
-    sys.exit(result)
-except Exception as e:
-    sys.exit(1)
-"; do
-  sleep 0.5
-done
-
 echo "PostgreSQL is up. Running migrations..."
 alembic upgrade head
 
