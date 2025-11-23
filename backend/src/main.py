7bd470ea142bb7304136b199aeb4288f74966a4e
Ralina <seitovaralina1@gmail.com>
2025-11-23 21:48:28 +0600

    feature: migrate the entire existing project to the backend folder + switch to poetry

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.routes import auth as auth_routes
-from src.routes import articles as articles_routes
-from src.routes import comments as comments_routes
+from src.routers import auth as auth_routes
+from src.routers import articles as articles_routes
+from src.routers import comments as comments_routes
 from src.core.errors.handlers import setup_exception_handlers
 
 app = FastAPI(title="Simple Blog API", version="1.0.0", description="API для управления пользователями, статьями и комментариями в блог-платформе.")
@@ -16,13 +16,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/health", tags=["health"],
-         summary="Проверить состояние сервиса", description="Возвращает статус работы сервиса.")
-def health():
-    return {"status": "ok"}
-
 setup_exception_handlers(app)
 
 app.include_router(auth_routes.router)
 app.include_router(articles_routes.router)
 app.include_router(comments_routes.router)
+
+@app.get("/health", tags=["health"],
+         summary="Проверить состояние сервиса", description="Возвращает статус работы сервиса.")
+def health():
+    return {"status": "ok"}
